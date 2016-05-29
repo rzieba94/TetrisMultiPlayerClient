b0cd41196bc2d7aea315d184c957d21645984957
Marcin Muskala <marcin.muskala@gmail.com>
2016-05-29 22:19:31 +0200

    changes in remote commands

--- TetrisMultiplayerClient/RemoteCmds.h
+++ TetrisMultiplayerClient/RemoteCmds.h
@@ -1,23 +1,26 @@
 #pragma once
-#include "stdafx.h"
+#include <string>
+#include "Cmds.h"
+#include "TetrominoType.h"
+#include "GameType.h"
 
 struct ClientHelloMsg
 {
-	string cmd;
+	Cmds cmd;
 	string nick;
 };
 
 struct ConnectionStatusMsg
 {
-	string cmd;
+	Cmds cmd;
 	string status;
 };
 
 // gracz wysyla chec ruchu, serwer odsyla potwierdzenie gdy ruch jest ok
 struct MoveMsg
 {
-	string cmd; //move
-	string moveType; //down, left, right, drop
+	Cmds cmd; //move
+	MoveType moveType; //down, left, right, drop
 	string userId; // nick
 	int dropCount; // 0 lub wartosc
 };
@@ -25,8 +28,8 @@ struct MoveMsg
 //gracz wysyla info aby serwer utworzyl watek gry, gdy gra sie rozpocznie (sa wszyscy uzytkownicy) serwer odsyla info o rozpoczeciu gry
 struct StartGame
 {
-	string cmd; // startGame
-	string gameType; // single, cooperation
+	Cmds cmd; // startGame
+	GameType gameType; // single, cooperation
 	int playersNumber; // liczba graczy na ktorych oczekuje
 	list<string> userIds; // lista id graczy
 };
@@ -34,15 +37,15 @@ struct StartGame
 //serwer wysyla info o tym ktora linia ma byc wyczyszczona
 struct ClearLine
 {
-	string cmd; //clearLine
+	Cmds cmd; //clearLine
 	int lineNumber; //numer linii
 };
 
 //serwer wysyla info gdzie umiescic tetromino, jakie i dla ktorego gracza
 struct PlaceTetromino
 {
-	string cmd; // placeTetromino
-	string tetrominoType; //typ tetromino
+	Cmds cmd; // placeTetromino
+	TetrominoType tetrominoType; //typ tetromino
 	string userId; // nick
 	int positionX;
 	int positionY;
@@ -51,22 +54,22 @@ struct PlaceTetromino
 //serwer wysyla: endGame, endServer, timeout(gdy czas oczekiwania na graczy przekroczony) klient wysy³a: getGamesList
 struct SimpleCommand
 {
-	string cmd;
+	Cmds cmd;
 };
 
 //serwer wysyla informacje o przyznaniu punktow
 struct AddScore
 {
-	string cmd; //addScore
+	Cmds cmd; //addScore
 	int score;
 };
 
 //serwer wysyla po otrzymaniu zapytania od gracza
 struct GamesList
 {
-	string cmd; //gamesList
+	Cmds cmd; //gamesList
 	list<int> gamesIds; // id gier oczekujacych na graczy
-	list<string> gameType; //typy gier oczekujacych na graczy
+	list<GameType> gameType; //typy gier oczekujacych na graczy
 };
 
 //klient wysyla aby polaczyc sie do istniejacej gry
