6d3f836d576163d3f4e106ee3152b20c063ebb31
Marcin Muskala <marcin.muskala@gmail.com>
2016-05-29 22:04:42 +0200

    changes in commands

--- TetrisMultiplayerClient/RemoteCmds.h
+++ TetrisMultiplayerClient/RemoteCmds.h
@@ -13,6 +13,7 @@ struct ConnectionStatusMsg
 	string status;
 };
 
+// gracz wysyla chec ruchu, serwer odsyla potwierdzenie gdy ruch jest ok
 struct MoveMsg
 {
 	string cmd; //move
@@ -21,27 +22,56 @@ struct MoveMsg
 	int dropCount; // 0 lub wartosc
 };
 
+//gracz wysyla info aby serwer utworzyl watek gry, gdy gra sie rozpocznie (sa wszyscy uzytkownicy) serwer odsyla info o rozpoczeciu gry
 struct StartGame
 {
 	string cmd; // startGame
 	string gameType; // single, cooperation
+	int playersNumber; // liczba graczy na ktorych oczekuje
+	list<string> userIds; // lista id graczy
 };
 
+//serwer wysyla info o tym ktora linia ma byc wyczyszczona
 struct ClearLine
 {
 	string cmd; //clearLine
-	int lineNumber; //numerLinii
+	int lineNumber; //numer linii
 };
 
+//serwer wysyla info gdzie umiescic tetromino, jakie i dla ktorego gracza
 struct PlaceTetromino
 {
 	string cmd; // placeTetromino
 	string tetrominoType; //typ tetromino
+	string userId; // nick
 	int positionX;
 	int positionY;
 };
 
-struct EndGame
+//serwer wysyla: endGame, endServer, timeout(gdy czas oczekiwania na graczy przekroczony) klient wysy³a: getGamesList
+struct SimpleCommand
 {
-	string cmd; // endGame
+	string cmd;
+};
+
+//serwer wysyla informacje o przyznaniu punktow
+struct AddScore
+{
+	string cmd; //addScore
+	int score;
+};
+
+//serwer wysyla po otrzymaniu zapytania od gracza
+struct GamesList
+{
+	string cmd; //gamesList
+	list<int> gamesIds; // id gier oczekujacych na graczy
+	list<string> gameType; //typy gier oczekujacych na graczy
+};
+
+//klient wysyla aby polaczyc sie do istniejacej gry
+struct ConnectToGame
+{
+	string cmd; //connectToGame
+	int gameId; //id gry
 };
